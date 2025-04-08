353d33c776f95f8b229bf421426ca311c0df4f50
ihavenocluewhothisnono <omarftb2k@gmail.com>
2025-04-08 15:35:50 -0400

    Update dataset_colorization.py

--- dataset/dataset_colorization.py
+++ dataset/dataset_colorization.py
@@ -15,30 +15,22 @@ class ColorizationDataset(Dataset):
       """
       self.root_dir = root_dir
       self.transform = transform
+
+      #Collects all the images from the file
       self.image_files = [f for f in os.listdir(root_dir) if f.lower().endswith(('png', 'jpg', 'jpeg'))]
-
-
 
 
   def __len__(self):
       return len(self.image_files)
 
-
-
-
   def __getitem__(self, idx):
       img_path = os.path.join(self.root_dir, self.image_files[idx])
-      rgb_img = Image.open(img_path).convert('RGB')
+      rgb_img = Image.open(img_path).convert('RGB') 
+    cana #converts the RGB image to grayscael as an input
       gray_img = rgb_img.convert('L')
-
-
-
 
       if self.transform:
           rgb_img = self.transform(rgb_img)
           gray_img = self.transform(gray_img)
-
-
-
 
       return gray_img, rgb_img, self.image_files[idx], self.image_files[idx]
